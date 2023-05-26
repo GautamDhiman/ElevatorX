54446915388b9a3e3546bd3d2ae951da0b1061c7
GautamDhiman <gautamCodesHere@gmail.com>
2023-05-26 14:29:09 +0530

    Edit elevator
    What?
    - auto increment elevator number

--- api/models/elevator.py
+++ api/models/elevator.py
@@ -16,6 +16,16 @@ class Elevator(BaseModel):
     def __str__(self):
         return self.elevator_number
 
+    def save(self, *args, **kwargs):
+        """overriding save method to auto increment elevator number."""
+
+        if not self.pk:
+            last_elevator_number = \
+                Elevator.objects.filter(elevator_system=self.elevator_system).aggregate(Max('elevator_number'))[
+                    'elevator_number__max']
+            self.elevator_number = last_elevator_number + 1 if last_elevator_number is not None else 1
+        super().save(*args, **kwargs)
+
     class Meta:
         """Meta class for Elevator"""
 
